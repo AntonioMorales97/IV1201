5d6e3ec21cd74f1faba2301b3b65c7edf203d73d
Gurk1992 <Gurk1992@users.noreply.github.com>
2020-01-27 20:52:46 +0100

    merging repositories

--- README.md
+++
@@ -1,2 +0,0 @@
-# IV1201 Design of Global Applications
-[![Build Status](https://travis-ci.org/AntonioMorales97/IV1201.svg?branch=master)](https://travis-ci.org/AntonioMorales97/IV1201)
--- api/server/routes/ApplicantRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ApplicantController from '../controllers/ApplicantController';
-
-const router = Router();
-
-router.get('/', ApplicantController.getAllApplicants);
-router.post('/', ApplicantController.addApplicant);
-
-export default router;
