9790da97cc48774a13a02016fabc5fb5c58eb44e
Antonio <antonio_97@live.se>
2020-02-07 12:31:00 +0100

    using jdbc postgres url

--- backend/src/main/java/se/kth/iv1201/recruitmentbackend/migration/integration/OldDbConnection.java
+++ backend/src/main/java/se/kth/iv1201/recruitmentbackend/migration/integration/OldDbConnection.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 
 
 public class OldDbConnection {
-	private final String dbUrl = System.getenv("HEROKU_POSTGRESQL_CRIMSON_URL"); //"jdbc:postgresql://localhost/old_recruitment?user=recruitment_acc&password=123123";
+	private final String dbUrl = System.getenv("HEROKU_POSTGRESQL_CRIMSON_JDBC_URL"); //"jdbc:postgresql://localhost/old_recruitment?user=recruitment_acc&password=123123";
 	private static final String SQL_DRIVER = "org.postgresql.Driver";
 	private Connection conn;
 	private static final Logger logger = LoggerFactory.getLogger(OldDbConnection.class);
@@ -22,7 +22,6 @@ public class OldDbConnection {
 			e.printStackTrace();
 			logger.error(e.getMessage());
 		}
-		System.out.println(System.getenv("HEROKU_POSTGRESQL_CRIMSON_URL"));
 		this.conn = DriverManager.getConnection(dbUrl);
 		this.conn.setAutoCommit(false);
 	}
