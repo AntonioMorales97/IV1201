3d802a28667407c494d0d57879cb47ff653bb528
Antonio <antonio_97@live.se>
2020-02-15 17:30:09 +0100

    removed unimplemented and unnecessary recruit page

--- frontend/client/src/components/pages/index.js
+++ frontend/client/src/components/pages/index.js
@@ -1,6 +1,5 @@
 import Login from './login';
 import Apply from './apply';
-import Recruit from './recruit';
 import Register from './register';
 
-export { Login, Apply, Recruit, Register };
+export { Login, Apply, Register };
--- frontend/client/src/routes/routes.js
+++ frontend/client/src/routes/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Login, Apply, Recruit, Register } from '../components/pages';
+import { Login, Apply, Register } from '../components/pages';
 import RecruitRoute from './private/recruit-route';
 import ApplicantRoute from './private/applicant-route';
 
@@ -12,7 +12,6 @@ const Routes = () => {
     <Switch>
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
-      <RecruitRoute exact path='/recruit' component={Recruit} />
       <ApplicantRoute exact path='/apply' component={Apply} />
       <RecruitRoute exact path='/applications/:id' component={Application} />
       <RecruitRoute exact path='/applications' component={Applications} />
